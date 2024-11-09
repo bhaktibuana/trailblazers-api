6465cb3133ef86fc2f436e39239c8ddb221852d6
Bhakti Mega Buana <bhaktibuana19@gmail.com>
2024-11-09 23:19:24 +0700

    create weth CA constant

--- src/shared/constants/web3.constant.ts
+++ src/shared/constants/web3.constant.ts
@@ -11,4 +11,12 @@ export class Web3Constant {
 		rpc_5: 'https://taiko-mainnet.rpc.porters.xyz/taiko-public',
 		rpc_6: 'https://taiko-rpc.publicnode.com',
 	};
+
+	public readonly CA_TESTNET = {
+		weth: '0xae2C46ddb314B9Ba743C6dEE4878F151881333D9',
+	};
+
+	public readonly CA_MAINNET = {
+		weth: '0xA51894664A773981C6C112C43ce576f315d5b1B6',
+	};
 }
