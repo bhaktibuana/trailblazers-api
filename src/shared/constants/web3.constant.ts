6ba49ac25ffdab12e9ffe45f2c3b0a576571f4c5
Bhakti Mega Buana <bhaktibuana19@gmail.com>
2024-11-09 23:17:35 +0700

    create taiko rpc constnats

---
+++ src/shared/constants/web3.constant.ts
@@ -0,0 +1,14 @@
+export class Web3Constant {
+	public readonly RPC_TESTNET = {
+		rpc_1: 'https://rpc.hekla.taiko.xyz',
+	};
+
+	public readonly RPC_MAINNET = {
+		rpc_1: 'https://rpc.mainnet.taiko.xyz',
+		rpc_2: 'https://rpc.ankr.com/taiko',
+		rpc_3: 'https://rpc.taiko.xyz',
+		rpc_4: 'https://taiko-json-rpc.stakely.io',
+		rpc_5: 'https://taiko-mainnet.rpc.porters.xyz/taiko-public',
+		rpc_6: 'https://taiko-rpc.publicnode.com',
+	};
+}
